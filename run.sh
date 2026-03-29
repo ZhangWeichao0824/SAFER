ddf5c8df005b81782cf3a7fc5ef8ad72c2b76d72
ZhangWeichao0824 <22281053@bjtu.edu.cn>
2026-03-29 17:09:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ done
 
 for dataset in  Reddit-CL Products-CL 
 do
-python train.py --dataset $dataset --method safer --backbone GCN --gpu 0 --ILmode classIL --inter-task-edges False --minibatch False
+python train.py --dataset $dataset --method safer --backbone GCN --gpu 0 --ILmode classIL --inter-task-edges False --minibatch True
 done
